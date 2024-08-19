b7525ab9c481247e9c16ebca3ef692e6a8ada8e9
Ted@Crestwood <ted@crestwood.co.ke>
2024-08-19 12:39:09 +0300

    end of subscription email

--- controllers/booking.controller.js
+++ controllers/booking.controller.js
@@ -1,5 +1,30 @@
 const bookingResponse = require("../mail/bookingResponse");
+const endBookingResponse = require("../mail/endBookingResponse");
 const Booking = require("../models/booking.model");
+
+
+const scheduleEmail = async (email) => {
+    const user = await Booking.findOne({ email })
+    const name = user.name;
+    const checkout = user.checkout;
+    try {
+        if (!checkout) {
+            return res.status(400).json({ message: "Failed to find checkout date" })
+        }
+        const date = checkout;
+        date.setMonth(date.getMonth() + 1);
+        const dayOfMonth = date.getDate();
+        cron.schedule(`1 8 ${dayOfMonth} * *`, async () => {
+            await endBookingResponse(email, name);
+        }, {
+            scheduled: true,
+            timezone: "Africa/Nairobi"
+        });
+    } catch (error) {
+        return res.status(500).json({ message: error.message })
+    }
+
+};
 
 const submitBooking = async (req, res) => {
     const { email, room, checkout, date, ...data } = req.body;
@@ -12,6 +37,7 @@ const submitBooking = async (req, res) => {
             return res.status(400).json({ message: "Error creating booking" })
         }
         await bookingResponse({ email, room, date })
+        await scheduleEmail(email)
         return res.status(200).json({ message: "Booking made successfully", info: { email, room, checkout } })
     } catch (error) {
         return res.status(500).json({ message: error.message })
