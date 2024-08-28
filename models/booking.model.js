f1b0d80656111f2d7aaebb70fc54daae73dbf3d6
Ted@Crestwood <ted@crestwood.co.ke>
2024-08-28 10:41:58 +0300

    added the guest and package to the booking model

--- models/booking.model.js
+++ models/booking.model.js
@@ -6,6 +6,8 @@ const BookingSchema = new mongoose.Schema({
     phone: { type: String, required: true }, // Changed to String for better handling
     subject: { type: String, required: true },
     room: { type: String, required: true },
+    package:{type:String},
+    guests:{type:String},
     date: { type: String }, // Changed to Date type
     checkIn: { type: String, required: true }, // Changed to Date type
     checkOut: { type: String, required: true }, // Changed to Date type
