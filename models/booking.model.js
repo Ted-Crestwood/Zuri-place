d4142f223efc7ed674c4e28294ab3ffdc427e37b
Ted@Crestwood <ted@crestwood.co.ke>
2024-08-30 15:51:21 +0300

    created a default message to fix error: {     "message": "Booking validation failed: message: Path `message` is required." }

--- models/booking.model.js
+++ models/booking.model.js
@@ -1,17 +1,17 @@
 const mongoose = require("mongoose");
 
 const BookingSchema = new mongoose.Schema({
-    name: { type: String},
+    name: { type: String },
     email: { type: String, unique: false },
-    phone: { type: String}, // Changed to String for better handling
-    subject: { type: String},
-    room: { type: String},
-    package:{type:String},
-    guests:{type:String},
+    phone: { type: String }, // Changed to String for better handling
+    subject: { type: String },
+    room: { type: String },
+    package: { type: String },
+    guests: { type: String },
     date: { type: String }, // Changed to Date type
-    checkIn: { type: String}, // Changed to Date type
-    checkOut: { type: String}, // Changed to Date type
-    message: { type: String},
+    checkIn: { type: String }, // Changed to Date type
+    checkOut: { type: String }, // Changed to Date type
+    message: { type: String, default: 'No message provided' },
     timeStamp: { type: String, default: Date.now() }
 });
 
